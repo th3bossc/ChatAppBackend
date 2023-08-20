3b3fd7311a3437bab616f8d9845f4dc3a4fdb702
Th3BossC <diljith2003@gmail.com>
2023-08-20 17:46:40 +0530

    debug in progress

--- controllers/userController.js
+++ controllers/userController.js
@@ -42,7 +42,9 @@ const loginHandler = expressAsyncHandler(async (req, res) => {
         res.status(400);
         throw new Error('all fields mandatory');
     }
+    console.log('access user started');
     const user = await prisma.user.findUnique({ where : { username } });
+    console.log('access user ended');
     if (!user) {
         res.status(400);
         throw new Error("User doesn't exist");
